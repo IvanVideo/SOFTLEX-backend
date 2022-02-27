7697c7735e925eba2d8a8e54f2dd9eeea44baca5
IvanVideo <lavochkaentuziastov@yandex.ru>
2022-02-27 21:56:14 +0300

    build: добавил роуты на создание пользователя и проверку токена

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 const { getItems } = require('../controllers/items');
 const { createItem } = require('../controllers/items');
+const { createUser } = require('../controllers/users');
+const { login } = require('../controllers/users');
+const { getUserInfo } = require('../controllers/users');
+const auth = require('../middlewares/auth');
 
 router.use('/items', getItems);
 router.post('/item', createItem);
+router.post('/signup', createUser);
+router.post('/signin', login);
+router.use('/check', auth, getUserInfo);
 
 module.exports = router;
